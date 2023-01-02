f7c45dbc1e2f979d32836da7961f69ada81c4b3d
Raphael Dias <71761453+vidadofael@users.noreply.github.com>
2023-01-02 16:00:36 -0300

    incluindo arquivos ao git

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Página de Cálculo de IMC com inserção básica de dados de peso e altura nos campos descritos
---
+++ dados.js
@@ -0,0 +1,19 @@
+
+function pulalinha() {
+
+    document.write("<br>");
+    document.write("<br>");
+}
+
+function mostra(frase) {
+    document.write(frase);
+    pulalinha();
+}
+
+var peso = 
+var altura = 
+
+var imc = peso/(altura*altura)
+
+mostra("O imc do " + nome + " é " + imc + ".");
+
